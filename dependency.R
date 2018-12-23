17a9d8e2f9ae61da5087160731f18f58af190659
KarthikeyanNarayanan <45782609+KarthikeyanNarayanan@users.noreply.github.com>
2018-12-23 14:06:45 +0530

    Add files via upload

---
+++ dependency.R
@@ -0,0 +1,14 @@
+if (!require(udpipe)){install.packages("udpipe")}
+if (!require(igraph)){install.packages("igraph")}
+if (!require(ggraph)){install.packages("ggraph")}
+if (!require(wordcloud)){install.packages("wordcloud")}
+if (!require(stringr)){install.packages("stringr")}
+
+library(udpipe)
+library(textrank)
+library(lattice)
+library(igraph)
+library(ggplot2)
+library(ggraph)
+library(wordcloud)
+library(stringr)

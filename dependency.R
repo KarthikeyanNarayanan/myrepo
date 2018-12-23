90497bc8971cbd984ee401b1983f994c2f068bac
KarthikeyanNarayanan <45782609+KarthikeyanNarayanan@users.noreply.github.com>
2018-12-23 14:17:02 +0530

    Add files via upload

---
+++ dependency.R
@@ -0,0 +1,17 @@
+try(require(shiny) || install.packages("shiny"))
+if (!require(udpipe)){install.packages("udpipe")}
+if (!require(ggraph)){install.packages("textrank")}
+if (!require(igraph)){install.packages("igraph")}
+if (!require(ggraph)){install.packages("ggraph")}
+if (!require(wordcloud)){install.packages("wordcloud")}
+if (!require(stringr)){install.packages("stringr")}
+
+library("shiny")
+library(udpipe)
+library(textrank)
+library(lattice)
+library(igraph)
+library(ggplot2)
+library(ggraph)
+library(wordcloud)
+library(stringr)
